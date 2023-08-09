457334f4b2f9e4d3eebf275f9a24d3c6d7984e5b
Amith Kini <kini.amit.22@gmail.com>
2023-08-09 01:26:00 -0500

    Fix new portfolio page

--- app/portfolio/new/page.tsx
+++ app/portfolio/new/page.tsx
@@ -1,20 +1,8 @@
-'use client'
-
-import PortfolioNav from '../portfolio-nav'
-import { useState, Suspense, useEffect } from 'react'
+import { Portfolio } from '@/db/schema/portfolio'
 
 export default function Portfolio({ params }: { params: { slug: string } }) {
-  const [showDialog, setShowDialog] = useState(false)
-
-  useEffect(() => {
-    setShowDialog(true)
-  }, [])
-
   return (
     <main>
-      <div className="flex flex-col">
-        <PortfolioNav portfolio={null} showDialog={showDialog} />
-      </div>
     </main>
   )
 }
