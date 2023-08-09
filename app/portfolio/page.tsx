d84b7e93333e4c613c9f5e543d40cfc8b8dab335
Amith Kini <kini.amit.22@gmail.com>
2023-08-08 20:12:10 -0500

    Fix issue with async portfolio loading

--- app/portfolio/page.tsx
+++ app/portfolio/page.tsx
@@ -9,24 +9,19 @@ import { Portfolio } from '@/db/schema/portfolio'
 
 export default function Portfolio() {
   const { push } = useRouter()
-  const [firstPortfolio, setFirstPortfolio] = useState<Portfolio | null>(null)
 
   useEffect(() => {
     // Fetch the portfolios and set the first one
     const loadData = async () => {
       const data = await fetch("/api/portfolio").then((res) => res.json());
-      setFirstPortfolio(data[0])
+      if (data[0]) {
+        push(`/portfolio/${data[0].slug}`)
+      } else {
+        push(`/portfolio/new`)
+      }
     }
     loadData();
   }, []);
-
-  useEffect(() => {
-    if (firstPortfolio) {
-      push(`/portfolio/${firstPortfolio.slug}`)
-    } else {
-      push(`/portfolio/new`)
-    }
-  }, [firstPortfolio]);
 
   return (
     <main>
